11606fee8beecebe215239f03ab17647ac4092e3
ikramerekabi <78406187+ikramerekabi@users.noreply.github.com>
2024-11-28 18:30:18 +0100

    Update main.c

--- door-locker-project/src/main.c
+++ door-locker-project/src/main.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "action_functions.h"
 #include "validation_functions.h"
 
 int main(int argc, char * argv[]) {
     int check;
-    int in0;
-    int in1;
+    int *in0 = malloc(sizeof(int) * 4);
+    int *in1 = malloc(sizeof(int) * 4);
 
     if (argc == 3) {
         check = validate(argv);
         if (check == 0) {
             puts("\nChecking values");
-            in0   = atoi(argv[2]);
-            in1   = atoi(argv[1]);
-            check = fnchck(in1, in0);
+            check = fnchck(&in1, &in0);
             if (check == 0xf) {
                 fngrt();
             } else {
